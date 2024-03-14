7688f8e2af0b673ec8058ae83214d4212943a37c
Shantanu Dutta <shantanubhs1985@gmail.com>
2024-03-14 13:10:54 +0530

    added register button

--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -3,6 +3,7 @@ import Navbar from "./Navbar.jsx";
 
 import "../Styles/Fonts.css"
 import "../Styles/Home.css";
+import { ArrowDownFromLine } from "lucide-react";
 
 const Banner = () => {
 
@@ -15,6 +16,13 @@ const Banner = () => {
       windowWidth: window.innerWidth,
     });
   };
+
+  const handleScrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+    }
+}
 
   useEffect(() => {
     window.addEventListener("resize", detectSize);
@@ -34,6 +42,11 @@ const Banner = () => {
       <Navbar/>
 
       <h1 className="font-[Simplicity] text-4xl md:text-[1] lg:text-8xl text-white z-0">PlayTopia</h1>
+
+
+      <a href="#events" className="flex justify-center rounded-lg w-[150px] bg-white h-[50px] my-3  items-center   cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#009b49] before:to-[rgb(105,184,141)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0  mt-[3rem] bg-gradient-to-r from-amber-500 to-pink-500 font-bold text-white " onClick={() => handleScrollToSection('events')}>
+                        Register <ArrowDownFromLine />
+                      </a>
       <div id="swipeDown">
           <div className="lines">
             <div className="line"></div>
@@ -49,6 +62,9 @@ const Banner = () => {
       <Navbar/>
 
       <h1 className="text-4xl font-[Simplicity] md:text-7xl lg:text-8xl text-white z-10">PlayTopia</h1>
+      <a href="#events" className="flex justify-center rounded-lg w-[150px] bg-white h-[50px] my-3  items-center   cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#009b49] before:to-[rgb(105,184,141)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0  mt-[3rem] bg-gradient-to-r from-amber-500 to-pink-500 font-bold text-white " onClick={() => handleScrollToSection('events')}>
+                        Register <ArrowDownFromLine />
+                      </a>
       <div id="swipeDown">
           <div className="lines">
             <div className="line"></div>
